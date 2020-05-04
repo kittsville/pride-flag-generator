7867a6dbd8be72630d18f3619a1247149852d332
Kit <kittsville@googlemail.com>
2020-05-04 15:21:39 +0100

    Support complex gradients
    Also rewrote colour calculation to handle red, green and blue separately, so that gradients actually make sense and aren't just pretty but useless (like me)

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,25 +1,92 @@
-const colorToHex = color => Number('0x' + color.substring(1));
-const hexToColor = hex => {
-  const c = Number(Math.floor(hex)).toString(16);
-  return '#' + ("000000".substr( 0, 6 - c.length ) + c.toUpperCase());
+class Color {
+  constructor(arg1, arg2, arg3) {
+    if (arg2 === undefined && arg3 === undefined) {
+      const hex = arg1;
+      const colorParts = hex.substring(1).match(/.{2}/g)
+
+      this.red = this.hexPartToColor(colorParts[0]);
+      this.green = this.hexPartToColor(colorParts[1]);
+      this.blue = this.hexPartToColor(colorParts[2]);
+    } else {
+      this.red = arg1;
+      this.green = arg2;
+      this.blue = arg3;
+    }
+  }
+
+  hexPartToColor(part) {
+    return Number('0x' + part);
+  }
+
+  colorToHexPart(color) {
+    const normalisedColor = Math.max(0, Math.min(255, Math.floor(color)));
+    const c = Number(normalisedColor).toString(16);
+    return "00".substr( 0, 2 - c.length ) + c.toUpperCase();
+  }
+
+  toString(){
+    const redHex = this.colorToHexPart(this.red);
+    const greenHex = this.colorToHexPart(this.green);
+    const blueHex = this.colorToHexPart(this.blue);
+
+    return `#${redHex}${greenHex}${blueHex}`;
+  }
+
+  add(color) {
+    return new Color(
+      this.red + color.red,
+      this.green + color.green,
+      this.blue + color.blue
+    );
+  }
+
+  multiply(number) {
+    return new Color(
+      this.red * number,
+      this.green * number,
+      this.blue * number
+    );
+  }
+
+  log() {
+    console.log('%c                       ', `background: ${this.toString()}`);
+  }
+
+  static gradient(firstColor, secondColor, numberOfParts) {
+    if (numberOfParts < 2) {
+      throw `Gradients require at least two parts, [${numberOfParts}] given'`
+    }
+
+    const colorDelta = new Color(
+      (secondColor.red - firstColor.red) / (numberOfParts - 1),
+      (secondColor.green - firstColor.green) / (numberOfParts - 1),
+      (secondColor.blue - firstColor.blue) / (numberOfParts - 1)
+    );
+
+    return Array.from({length: numberOfParts}, (_, i) => firstColor.add(colorDelta.multiply(i)));
+  }
+
+  static twoPartGradient(firstColor, secondColor, thirdColor, numberOfParts) {
+    const firstGradient = Color.gradient(firstColor, secondColor, numberOfParts);
+    const secondGradient = Color.gradient(secondColor, thirdColor, numberOfParts);
+
+    return firstGradient.concat(secondGradient.slice(1));
+  }
 }
 
 class Bar {
-  constructor(fillStyle) {
-    this.fillStyle = fillStyle;
+  constructor(color) {
+    this.color = color;
   }
 
-  static gradient(firstStyle, lastStyle, numberOfBars) {
-    const firstStyleHex = colorToHex(firstStyle);
-    const lastStyleHex = colorToHex(lastStyle);
-    const colorDelta = (lastStyleHex - firstStyleHex) / (numberOfBars - 1);
+  static gradient(firstColor, secondColor, numberOfBars) {
+    return Color.gradient(firstColor, secondColor, numberOfBars)
+            .map(color => new Bar(color));
+  }
 
-    console.log(hexToColor(firstStyleHex));
-
-    return Array.from({length: numberOfBars}, (_, i) => {
-      const color = hexToColor(firstStyleHex + (colorDelta * i));
-      return new Bar(color);
-    });
+  static twoPartGradient(firstColor, secondColor, thirdColor, numberOfBars) {
+    return Color.twoPartGradient(firstColor, secondColor, thirdColor, numberOfBars)
+            .map(color => new Bar(color));
   }
 }
 
@@ -36,21 +103,21 @@ function render(flag) {
   const barHeight = canvas.height / flag.bars.length;
 
   flag.bars.forEach((bar, i) => {
-    ctx.fillStyle = bar.fillStyle;
+    ctx.fillStyle = bar.color.toString();
 
     const x1 = 0;
     const x2 = canvas.width;
     const y1 = barHeight * i;
     const y2 = y1 + barHeight;
 
-    console.log(`Filling bar with ${bar.fillStyle} at ${x1}, ${y1}, ${x2}, ${y2}`);
+    console.log(`Filling bar with ${ctx.fillStyle} at ${x1}, ${y1}, ${x2}, ${y2}`);
 
     ctx.fillRect(x1, y1, x2, y2);
   });
 }
 
 const bars =
-  Bar.gradient('#a8e2b0', '#edc8ea', 6);
+  Bar.twoPartGradient(new Color('#edc8ea'), new Color('#ffffff'), new Color('#a8e2b0'), 3);
 const flag = new Flag(bars);
 render(flag);
 
