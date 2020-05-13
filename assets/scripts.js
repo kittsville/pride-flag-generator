kittsville/pride-flag-generator

commit ee618a4f5c1cccb60b70d8675c89d43391c8508c

Refactor

assets/scripts.js
@@ -9,7 +9,7 @@ function render(flag) {
   const barHeight = canvas.height / flag.bars.map(bar => bar.height).reduce((a, b) => a + b);
 
   flag.bars.reduce((i, bar) => {
-    ctx.fillStyle = bar.color.toHex();
+    ctx.fillStyle = bar.color;
 
     const x1 = 0;
     const x2 = canvas.width;
@@ -24,16 +24,12 @@ function render(flag) {
   }, 0);
 
   const arrowWidth = Math.sqrt((barHeight / 2) ** 2 + (barHeight / 2) ** 2);
-  const arrowStart = (flag.arrows.length - 1) * (-arrowWidth/2)
-
-  console.log(arrowStart);
+  const arrowStart = (flag.arrows.length - 1) * (-arrowWidth / 2);
 
   flag.arrows.forEach((arrow, i) => {
-    ctx.fillStyle = arrow.color.toHex();
+    ctx.fillStyle = arrow.color;
 
-    const arrowOffset = arrowStart + (arrowWidth * (flag.arrows.length - 1 - i))
-
-    console.log(arrowOffset);
+    const arrowOffset = arrowStart + (arrowWidth * (flag.arrows.length - 1 - i));
 
     const x1 = 0;
     const y1 = -arrowOffset;

assets/definitions.js
@@ -1,32 +1,6 @@
-class ColorDelta {
-  constructor(red, green, blue) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
-  }
-
-  multiply(number) {
-    return new ColorDelta(
-      this.red * number,
-      this.green * number,
-      this.blue * number
-    );
-  }
-
-  applyTo(color) {
-    const rgbColor = color.toRgbObject();
-
-    return colr.fromRgb(
-      Math.max(0, Math.min(255, rgbColor.r + this.red)),
-      Math.max(0, Math.min(255, rgbColor.g + this.green)),
-      Math.max(0, Math.min(255, rgbColor.b + this.blue))
-    );
-  }
-}
-
 class ColorTools {
   static log(color) {
-    console.log('%c                       ', `background: ${color.toHex()}`);
+    console.log('%c                       ', `background: ${color}`);
   }
 
   static gradient(firstColor, secondColor, numberOfParts) {
@@ -34,13 +8,10 @@ class ColorTools {
       throw `Gradients require at least two parts, [${numberOfParts}] given'`
     }
 
-    const firstColorHsl = firstColor.toRawHslObject();
-    const secondColorHsl = secondColor.toRawHslObject();
-
-    const scale = chroma.scale([firstColorHsl, secondColorHsl]).mode('lab');
+    const scale = chroma.scale([firstColor, secondColor]).mode('lab');
     const step = 1 / (numberOfParts - 1);
 
-    return Array.from({length: numberOfParts}, (_, i) => colr.fromRgbArray(scale(step * i).rgb(false)));
+    return Array.from({length: numberOfParts}, (_, i) => scale(step * i).hex());
   }
 
   static twoPartGradient(firstColor, secondColor, thirdColor, numberOfParts) {
@@ -78,7 +49,7 @@ class ColorTools {
       console.log(`Gave up generating distinctive colours after ${attempts} attempts`);
     }
 
-    return pastels.map(hue => colr.fromHsl(hue, 70, 80));
+    return pastels.map(hue => `hsl(${hue},70%,80%)`);
   }
 }
 

assets/flags.js
@@ -1,44 +1,44 @@
 document.getElementById('original-pride').addEventListener('click', () => {
   const bars = [
-    new Bar(colr.fromHex("#ff69b4")),
-    new Bar(colr.fromHex("f00")),
-    new Bar(colr.fromHex("#ff8e00")),
-    new Bar(colr.fromHex("#ff0")),
-    new Bar(colr.fromHex("#008e00")),
-    new Bar(colr.fromHex("#00c0c0")),
-    new Bar(colr.fromHex("#400098")),
-    new Bar(colr.fromHex("#8e008e"))
+    new Bar("#ff69b4"),
+    new Bar("f00"),
+    new Bar("#ff8e00"),
+    new Bar("#ff0"),
+    new Bar("#008e00"),
+    new Bar("#00c0c0"),
+    new Bar("#400098"),
+    new Bar("#8e008e")
   ];
   render(new Flag([3,2], bars));
 });
 
 document.getElementById('pride').addEventListener('click', () => {
   const bars = [
-    new Bar(colr.fromHex("#E40303")),
-    new Bar(colr.fromHex("#FF8C00")),
-    new Bar(colr.fromHex("#FFED00")),
-    new Bar(colr.fromHex("#008026")),
-    new Bar(colr.fromHex("#004DFF")),
-    new Bar(colr.fromHex("#750787"))
+    new Bar("#E40303"),
+    new Bar("#FF8C00"),
+    new Bar("#FFED00"),
+    new Bar("#008026"),
+    new Bar("#004DFF"),
+    new Bar("#750787")
   ];
   render(new Flag([3,5], bars));
 });
 
 document.getElementById('progress-pride').addEventListener('click', () => {
   const bars = [
-    new Bar(colr.fromHex("#E40303")),
-    new Bar(colr.fromHex("#FF8C00")),
-    new Bar(colr.fromHex("#FFED00")),
-    new Bar(colr.fromHex("#008026")),
-    new Bar(colr.fromHex("#004DFF")),
-    new Bar(colr.fromHex("#750787"))
+    new Bar("#E40303"),
+    new Bar("#FF8C00"),
+    new Bar("#FFED00"),
+    new Bar("#008026"),
+    new Bar("#004DFF"),
+    new Bar("#750787")
   ];
   const arrows = [
-    new Shape(colr.fromHex("#000")),
-    new Shape(colr.fromHex("#784F17")),
-    new Shape(colr.fromHex("#5BCEFA")),
-    new Shape(colr.fromHex("#F5A9B8")),
-    new Shape(colr.fromHex("#FFF"))
+    new Shape("#000"),
+    new Shape("#784F17"),
+    new Shape("#5BCEFA"),
+    new Shape("#F5A9B8"),
+    new Shape("#FFF")
 
   ];
   render(new Flag([3,5], bars, arrows));
@@ -46,12 +46,12 @@ document.getElementById('progress-pride').addEventListener('click', () => {
 
 document.getElementById('demi-pride').addEventListener('click', () => {
   const bars = [
-    new Bar(colr.fromHex("#FFF"), 3),
-    new Bar(colr.fromHex("#6E0070"), 1),
-    new Bar(colr.fromHex("#D2D2D2"), 3)
+    new Bar("#FFF", 3),
+    new Bar("#6E0070", 1),
+    new Bar("#D2D2D2", 3)
   ];
   const arrows = [
-    new Shape(colr.fromHex("#000"))
+    new Shape("#000")
 
   ];
   render(new Flag([3,5], bars, arrows));
@@ -59,9 +59,9 @@ document.getElementById('demi-pride').addEventListener('click', () => {
 
 document.getElementById('bi-pride').addEventListener('click', () => {
   const bars = [
-    new Bar(colr.fromHex("#0038a8"), 2),
-    new Bar(colr.fromHex("#9b4f96"), 1),
-    new Bar(colr.fromHex("#d60270"), 2)
+    new Bar("#0038a8", 2),
+    new Bar("#9b4f96", 1),
+    new Bar("#d60270", 2)
   ];
   render(new Flag([3,5], bars));
 });

index.html
@@ -88,7 +88,6 @@
     <div>
       <script src="/assets/material-components-web.min.js"></script>
       <script src="/assets/chroma.min.js"></script>
-      <script src="/assets/colr.min.js"></script>
       <script src="/assets/definitions.js"></script>
       <script src="/assets/scripts.js"></script>
       <script src="/assets/flags.js"></script>

commit d920181be7d06574df4013bf15a81038246f902e

Fix bug in random symetrical flags
Introduced in ee618a4f5c1cccb60b70d8675c89d43391c8508c

assets/scripts.js
@@ -79,7 +79,7 @@ document.getElementById('symetrical-color-pastel').addEventListener('click', () 
   const colors = ColorTools.randomPastels(2);
 
   const outerColor = colors[0]
-  const innerColor = Math.random() >= .8 ? colr.fromGrayscale(255) : colors[1];
+  const innerColor = Math.random() >= .8 ? '#FFF' : colors[1];
 
   const bars = Bar.twoPartGradient(
     outerColor,